84021182e76bf0f0983c287405fbbb148d08562e
dejank21 <181667145+dejank21@users.noreply.github.com>
2025-04-19 14:38:13 -0500

    added a regex to split the string properly

--- substrings.rb
+++ substrings.rb
@@ -1,6 +1,6 @@
 def substrings(string, dictionary)
   #Take the provided string and split it into lowercase words for comparison as well as create an empty hash to store the results
-  string_words = string.downcase.split
+  string_words = string.downcase.split(/[^a-z0-9]/)
   result = Hash.new(0)
   #iterates though each word and sees if it present in the dictionary, if it is then increase the value by 1.
   string_words.each do |word|
@@ -12,4 +12,4 @@ def substrings(string, dictionary)
 end
 
 dictionary = ['take','on','me','how','are','you','or','doing','do','want','to']
-puts substrings("How are you doing, do you want to take me on or take on me", dictionary)
+puts substrings("How are you doing, do you want to take me on or take on me?", dictionary)
