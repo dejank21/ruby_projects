efc7225d9c63d0c67f5bbf8ebe44898abf9d9aac
dejank21 <181667145+dejank21@users.noreply.github.com>
2025-04-25 20:04:22 -0500

    Fixed a typo

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,6 +1,6 @@
 def stock_picker(price_array) #This method returns the best day to buy and the best day to sell from a provided list of prices, with each price representing one day.
   
-  return [] if price_array.length < 2 #this returns an empty array if there are less than two values since this is our minimum for a buy and sell day
+  return [] if price_array.length < 2 #This returns an empty array if there are less than two values since this is our minimum for a buy and sell day
   
   #Here we set the default for the best days to buy and sell.
   best_days = [0, 1]
