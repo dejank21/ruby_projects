3bb72b2da0aaff9665f68a3d3e1d2b753aea9167
dejank21 <181667145+dejank21@users.noreply.github.com>
2025-04-25 20:01:12 -0500

    Finished flow and overall method

--- stock_picker.rb
+++ stock_picker.rb
@@ -2,15 +2,21 @@ def stock_picker(price_array) #This method returns the best day to buy and the b
   
   return [] if price_array.length < 2 #this returns an empty array if there are less than two values since this is our minimum for a buy and sell day
   
-  #Here we set the initial buy and sell day index as well as calculate the difference between the two values.
-  buy_day = 0
-  sell_day = 1
-  max_difference = price_array[sell_day] - price_array[buy_day]
+  #Here we set the default for the best days to buy and sell.
+  best_days = [0, 1]
+  best_profit = price_array[1] - price_array[0]
   
-  price_array.each_with_index do |buy_price, current_buy_day| #each_with_index uses two inputs |value, index|
-    ((current_buy_day + 1)...price_array.length).each do |current_sell_day| # ... is used to to ensure length uses an appropriate value since the first index is technically 0 which is not valid with the .length method
-      #This is  
+  0.upto(price_array.length - 2) do |buy_day| #This specifies the number of times we will iterate to determine a buy day
+    (buy_day + 1).upto(price_array.length - 1) do |sell_day| #This uses each buy day to iterate up to the last value in the array as a possible sell day.
+      profit = price_array[sell_day] - price_array[buy_day]
+      
+      if profit > best_profit
+        best_profit = profit
+        best_days = [buy_day, sell_day]
+      end
     end
+  end
+  return best_days
 end
 
 puts stock_picker([6,11,24,4,2,69,22,10,45])
