2ab3bd2bb5d774a30fc3ff3ab13abc02ba58a6f9
dejank21 <181667145+dejank21@users.noreply.github.com>
2025-05-01 08:44:52 -0500

    Added some comments to the main.rb file

--- test_project/main.rb
+++ test_project/main.rb
@@ -1,8 +1,9 @@
 require_relative 'lib/airport'
 require_relative 'lib/flight'
 require_relative 'lib/hotel'
-#Here we are using require_relative to incluede my classes, if we had dependecy files we would use require.
-
+# Here we are using require_relative to include my classes, if we had dependecy files we would use require.
+# If two classes use methods with the same name, rubt will default to the last one loaded.
+# We avoid this using modules.
 Airport.new.introduce
 
 Flight.new.introduce
